cee992717bab7955515c87893aeb0300bf495e11
nalain-dot <nalainabbas348@gmail.com>
2025-05-23 13:58:19 +0500

    cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,12 +1,13 @@
-import {v2 as cloudinary} from 'cloudinary'
+// config/cloudinary.js
+import { v2 as cloudinary } from 'cloudinary';
+import dotenv from 'dotenv';
 
-const connectCloudinary = async () => {
-   cloudinary.config({
-    cloud_name : process.env.CLOUDINARY_NAME,
-    api_key : process.env.CLOUDINARY_API_KEY,
-    api_secret : process.env.CLOUDINARY_SECRET_KEY
-   })
-   console.log('Cloudinary connected')
-}
+dotenv.config(); // load .env
 
-export default connectCloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY
+});
+
+export default cloudinary; // 🔥 This is what was missing!
