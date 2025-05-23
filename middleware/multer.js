f4c1cade054b5911a7c6b55db6edf2750d46e50e
nalain-dot <nalainabbas348@gmail.com>
2025-05-23 13:59:00 +0500

    multer.js

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,11 +1,14 @@
 import multer from "multer";
+import fs from "fs";
+import path from "path";
+
+const uploadFolder = path.join(process.cwd(), "uploads", "hero");
+fs.mkdirSync(uploadFolder, { recursive: true });
 
 const storage = multer.diskStorage({
-    filename:function(req,file,callback){
-        callback(null,file.originalname)
-    }
-})
+  destination: (req, file, cb) => cb(null, uploadFolder),
+  filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`),
+});
 
-const upload = multer({storage})
-
-export default upload
+const upload = multer({ storage });
+export default upload;
